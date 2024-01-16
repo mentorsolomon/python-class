0358ffb1a8aaf30a6112533706afdba33226cb19
Adedokun Solomon <skadedokun16@gmail.com>
2024-01-16 16:05:28 +0100

    six v3

--- sets.py
+++ sets.py
@@ -5,9 +5,25 @@
 # set: A SET IS A COLLECTION WHICH IS UNORDERED, UNINDEXED, IMMUTABLE/UNCHANGEABLE AND DOES NOT ACCEPT DUPLICATE VALUE
 # set {} or ()
 
-name = {"james", "sade", "larry", "gaga", "jerry", "ige"}
+# name = {"james", "sade", "larry", "gaga", "jerry", "ige"}
 
-setNames = set(('Muazeem', 'Ige', 'Lado'))
+# setNames = set(('Muazeem', 'Ige', 'Lado'))
 
-setNames = ('Muazeem', 'Ige', 'Lado')
+# setNames = ('Muazeem', 'Ige', 'Lado')
 
+sets = set()
+print('welcome to set calculator')
+print('how many sets are you working with?')
+numSets= int(input('no of sets > '))
+for name_of_set in range(numSets):
+    setItems = input(f'Your set Items {name_of_set + 1}: ')
+    sets.update(setItems)
+    # 
+print(sets)
+    
+
+
+# no_of_setItems = int(input('how many item are in your set?: '))
+#     # for setItems in no_of_setItems:
+#     #     setItem = int(input('list the numbers in your set: '))
+#     # print(setItem)
