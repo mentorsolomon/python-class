c1ff6234181fade65ad564dd6d542cd9391fa835
Adedokun Solomon <skadedokun16@gmail.com>
2024-01-23 12:38:22 +0100

    seven v3

---
+++ classwork.py
@@ -0,0 +1,115 @@
+def land():
+    ussdCode = input('Enter the ussd code here: ')
+    if ussdCode != '*312#':
+        print('Invalid USSD code entered')
+        land()
+    else:
+        offers()
+
+
+def offers():
+    print('''
+        Welcome
+        
+        1. My Offer
+        2. My Area
+        3. Data Plans
+        4. N5000/18GB/30dys
+        5. N3000/10GB/30dys
+        6. N1500/5GB/7Days
+        7. N500/2GB/2Days
+        8. SMART DATA PLANS
+        9. Family Plans
+        99. press for next page
+        *
+        
+        
+        ''')
+
+    user = int(input('Select: '))
+    if user == 1:
+        my_offer()
+    elif user == 2:
+        area_offer()
+    elif user == 3:
+        data_plan()
+    elif user == 99:
+        next_page()
+    else:
+        print('Invalid request enter a valid key')
+        offers()
+        
+                
+
+def my_offer():
+    print("""
+        My Airtel Offer
+        
+        1. N500 gives N2500 for calls
+        2. N700 gives N3500 for calls
+        3. MORE VOICE OFFER
+        4. DATA OFFER
+        5. RECHARGE OFFER
+        22. Back
+        0. Menu
+                            
+        """)
+    userOne = int(input('Put a number here: '))
+    if userOne == 22:
+        offers()
+        
+def area_offer():
+     print('''
+        AREA OFFERS
+        
+        1. N3000/20GB/30days
+        2. N5000/40GB/30days
+        3. 500/30MINS/1.5GB/7DAYS
+        4. 1000/70mins/3GB/15days
+        22. Back
+        0. menu    
+                    
+                    ''')
+                
+     userTwo = int(input('Put a number here: '))
+     if userTwo == 22:
+        offers()
+    
+def data_plan():
+     print("""
+                    
+    1. Daily/Weekly Bundles
+    2. Weekly Bundles
+    3. Monthly Bundles
+    4. More Data (Data++)
+    5. Mega Bundles
+    6. Big Data Bundles
+    *. Next
+    22. Back
+    0. Menu            
+    
+                    """)
+     
+     userThree = int(input('Put a number here: '))
+     if userThree == 22:
+        offers()
+                
+def next_page():
+    print("""
+                                    
+    10. Everyday ON
+    11. super Binge
+    12. Social Bundle
+    13. Hourly Plan
+    14. Gifting & Sharing                  
+    15. Trybe Data Bundles
+    16. Data Balance
+    0. Main Menu            
+                    
+                    """)
+    userNineNine = int(input('Put a number here: '))
+    if userNineNine == 0:
+        offers()
+    
+land()
+
--- functions.py
+++ functions.py
@@ -1,5 +1,112 @@
 # JANUARY 22ND BE MOVING WITH MAD SPEED
 # functionS
 
-# ALLOWS SOURCE TO REPEAT ITSELF MULTIPLE TIMES
+# ALLOWS SOURCE TO REPEAT ITSELF MULTIPLE TIMES , # object: has a property anc can perform a function
 
+# God[creator], man[creation/object], dust[mode/class].
+
+# you create a function with def i.e function or definition 
+# types of functionS
+# parametized and unparametized 
+# arguments occur inside the parenthesis (argument comes in here)
+# ====================================SOLVED EXAMPLES===========================================================
+# unparametized
+# def addition():
+#     """Yo, I add thing up!!"""
+    
+# addition() #invocation : calling of the created func
+# # parametized
+# def addition(val1, val2):
+#     """Yo, I add thing up!!"""
+#     print(val1 + val2)
+# # addition(90, 99)
+
+# val1 = int(input("> "))
+# val2 = int(input("> "))
+
+# def addition():
+    
+    
+#     print(val1 + val2)
+#     subt()
+    
+# def subt():
+#     print(val1 - val2)
+    
+# addition()
+    
+# ===========================================
+numList = []
+def land():
+    print('Welcome, what would you like to do?')
+    choice = input('add, sub, div, mult, exit: ')
+    print(f'you selected the {choice} funtion')
+    if choice == 'add':
+        add()
+    elif choice == 'sub':
+        sub() 
+
+    elif choice == 'div':
+        div()
+    
+    elif choice == 'mult':
+        mult()
+        
+    elif choice.lower().strip() == 'exit':
+        exit()
+        
+    else:
+        print('error')
+        land()
+
+def add():
+    '''I add all entries'''
+    entry = int(input('How many number do you want to add: '))       
+    numList = [int(input(f'\n Enter the number {num + 1}> ')) for num in range(entry)]
+    print(f'\n{numList}\n')
+    print(sum(numList))
+    land()
+    
+def sub():
+    '''I subtract entry 2 from 1: make sure 1 is solely higher else'''
+    entry = int(input('How many number do you want to sub: '))       
+    numList = [int(input(f'\n Enter the number {num + 1}> ')) for num in range(entry)]
+    print(f'\n{numList}\n')
+    print((numList[0] - numList[1]))
+    land()
+def div():
+    '''I divide entry 1 by entry 2'''
+    entry = int(input('How many number do you want to divide: '))       
+    numList = [int(input(f'\n Enter the number {num + 1}> ')) for num in range(entry)]
+    print(f'\n{numList}\n')
+    print((numList[0] / numList[1]))
+    land()
+
+def mult():
+    '''I multiply entry 1 and 2'''
+    entry = int(input('How many number do you want to multiply: '))       
+    numList = [int(input(f'\n Enter the number {num + 1}> ')) for num in range(entry)]
+    print(f'\n{numList}\n')
+    print(numList[0] * numList[1])
+    land()
+
+land()
+
+# tuple = ('james','DAVID','larry')
+# print(tuple)
+# *l, = tuple
+# print(l)
+
+# # ======================================lambda-------------------
+# add = lambda x, y: print(x+y)
+# add(2,4)
+
+# =======ARGS ABD KWARGS=============================JANUARY 23RD 2024 CLASS WORK===============================
+def dealer(product, *toppings, **details): #*args, **kwargs
+    print(f'You are about to purchase {product}. You would also be going with:')
+    for topping in toppings:
+        print(f'--> {topping}')
+    
+    
+    
+dealer('Car', ' 2 Tyres', 'Extinguisher', 'Engine Oil', 'Windscreen', model = 'Lexus', Year = 2020, maker = 'Toyota companies')
