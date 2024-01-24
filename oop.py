49010499a0224aaf7da10f280fa81c232a9c0ad8
Adedokun Solomon <skadedokun16@gmail.com>
2024-01-24 12:08:22 +0100

    seven v6

---
+++ oop.py
@@ -0,0 +1,44 @@
+# JANUARY 24TH 2024
+# OOP === OBJECT ORIENTED LANGUAGE//// ALWAYS INITIALIZE YOUR CLASS
+
+numbers = list()
+# numbers.sort
+# print(numbers)
+
+
+# class Dust:
+#     def __init__(self, firstName):  #SELF here is a tag that references the clas
+        
+#         self.creation = 'Human'
+#         self.lastName = 'Martin'
+#         self.firstName = firstName
+        
+#         self.walk()
+    
+#     def walk(self):
+#         '''what i do is WALK'''
+#         print(f'{self.creation} can walk')
+
+# Solomon = Dust()
+
+# Solomon.walk()
+
+# ====================================================================
+class USSD:
+    def __init__(self):
+        
+        self.home()
+
+    def home(self):
+        self.ussd = input('Enter USSD here > ').strip()
+        while self.ussd != "*312#":
+            print('Invalid Code!')
+            self.home()
+        self.landingPage()
+    def landingPage(self):
+        print('''
+              1. Buy Data
+              2. Check Balance
+              ''')
+        
+ussd = USSD
--- todo.py
+++ todo.py
@@ -1,143 +1,145 @@
-# toDo = []
+toDo = []
 
-# print('Here you have your TO-DO LIST'.center(70,'_'))
-# def home():
-#     print('''
-#           1. Create a List
-#           2. Exit
-#           ''')
-#     user = input(' What is it you want to do > ')
-#     if user == '1':
-#         land()
-#     elif user == '2':
-#         exit()
-#     else:
-#         print('Try Again.')
-#         home()
-# def land():
-#     print('''
-#         1. add to list
-#         2. Edit Item
-#         3. Display list
-#         4. delete item
-#         5. delete list
-#         6. exit
-#             ''')
-#     user = input('What do you want to do?: >').strip().lower()
-#     if user == '1' or user == 'add':
-#         add()
-#     elif user == '2' or user == 'edit':
-#         edit()
-#     elif user == '3' or user == 'display':
-#         display()
-#     elif user == '4' or user == 'remove':
-#         del_item()
-#     elif user == '5' or user == 'delete':
-#         delete()
-#     elif user == '6' or user == 'exit':
-#         exit()
-#     else:
-#         print('Invalid selection. Try Again')
-#         land()
-        
-# def add():
-#     no_of_items = int(input('How many items are you doing today. [1,2,3...10]. Think about it clearly > '))
-#     for num in range(no_of_items):
-#         tasks =  input(f'Using Action words, what are you doing today. Task {num + 1}  > ').strip().title()
-#         toDo.append(tasks)
-#     land()
-
-# def edit():
-#     print(toDo)
-#     nums = int(input('Change your to do list at number. > '))
-#     place = nums - 1
-#     toDo[place] = input('Enter new item to do: ').title()
-#     print(toDo)
-#     land()
-    
-#     # OR 
-#     # print(toDo)
-#     # you_edit = input('What do you want to change > ')
-#     # toDo.index(you_edit) = input('The new entry: ')
-#     # print(toDo)
-#     # land()=
-
-# def display():
-#     for every in toDo:
-#         print(f'\n{every}')
-#     land()
-
-# def del_item():
-#     remove()
-    
-    
-# def remove():
-#     print(toDo)
-#     user = input('What will you be removing: ').title().strip()
-#     if user in toDo:
-#         toDo.remove(user)
-#         land()
-#     else:
-#         print('Entry not found on List')
-#         del_item()
-
-# def delete():
-#     toDo.clear()
-#     print(f"Cleared Successfully. List ({toDo}) is now empty. ")
-#     land()
-    
-# land()
-
-# ==========================correctio==========================
-def toDo():
+print('Here you have your TO-DO LIST'.center(70,'_'))
+def home():
     print('''
-          My ToDo APP
-          
-    1. Create ToDo
-    2. Exit
-          
+          1. Create a List
+          2. Exit
           ''')
-    choice = input('Option: ')
-    if choice == '1':
-       todoList = []
-       create(todoList)
-    elif choice == '2':
+    user = input(' What is it you want to do > ')
+    if user == '1':
+        add()
+    elif user == '2':
         exit()
     else:
-        print('Invalid Input. Try Again')
-        toDo()
+        print('Try Again.')
+        home()
+
+
+def land():
+    print('''
+        1. add to list
+        2. Edit Item
+        3. Display list
+        4. delete item
+        5. delete list
+        6. exit
+            ''')
+    user = input('What do you want to do?: >').strip().lower()
+    if user == '1' or user == 'add':
+        add()
+    elif user == '2' or user == 'edit':
+        edit()
+    elif user == '3' or user == 'display':
+        display()
+    elif user == '4' or user == 'remove':
+        del_item()
+    elif user == '5' or user == 'delete':
+        delete()
+    elif user == '6' or user == 'exit':
+        exit()
+    else:
+        print('Invalid selection. Try Again')
+        land()
+        
+def add():
+    no_of_items = int(input('How many items are you doing today. [1,2,3...10]. Think about it clearly > '))
+    for num in range(no_of_items):
+        tasks =  input(f'Using Action words, what are you doing today. Task {num + 1}  > ').strip().title()
+        toDo.append(tasks)
+    land()
+
+def edit():
+    print(toDo)
+    nums = int(input('Change your to do list at number. > '))
+    place = nums - 1
+    toDo[place] = input('Enter new item to do: ').title()
+    print(toDo)
+    land()
     
-def create(value:list):
-    # global todoList
-    # todoList = []
-    while True:
-        user = input("Input Task. (Press '1' to stop adding task): ").strip()
-        if user == '1':
-            home(value)
+    # OR 
+    # print(toDo)
+    # you_edit = input('What do you want to change > ')
+    # toDo.index(you_edit) = input('The new entry: ')
+    # print(toDo)
+    # land()=
+
+def display():
+    for every in toDo:
+        print(f'\n{every}')
+    land()
+
+def del_item():
+    remove()
+    
+    
+def remove():
+    print(toDo)
+    user = input('What will you be removing: ').title().strip()
+    if user in toDo:
+        toDo.remove(user)
+        land()
+    else:
+        print('Entry not found on List')
+        del_item()
+
+def delete():
+    toDo.clear()
+    print(f"Cleared Successfully. List ({toDo}) is now empty. ")
+    land()
+    
+home()
+
+# ==========================correctio==========================
+# def toDo():
+#     print('''
+#           My ToDo APP
+          
+#     1. Create ToDo
+#     2. Exit
+          
+#           ''')
+#     choice = input('Option: ')
+#     if choice == '1':
+#        todoList = []
+#        create(todoList)
+#     elif choice == '2':
+#         exit()
+#     else:
+#         print('Invalid Input. Try Again')
+#         toDo()
+    
+# def create(value:list):
+#     # global todoList
+#     # todoList = []
+#     while True:
+#         user = input("Input Task. (Press '1' to stop adding task): ").strip()
+#         if user == '1':
+#             home(value)
             
-        value.append(user)
+#         value.append(user)
     
 
-def home(values:list):
-    print('''
-          1. View Todo
-          2. Edit Item in Todo
-          3. Delete Item in Todo
-          4. Delete Todo
-          5. Add to Todo
+# def home(values:list):
+#     print('''
+#           1. View Todo
+#           2. Edit Item in Todo
+#           3. Delete Item in Todo
+#           4. Delete Todo
+#           5. Add to Todo
                     
-          ''')    
-    user = input('Option > ')
-    if user == '1':
-        for num, value in enumerate(values, start=1):
-            print(f'''
-                  {num}. {value}
-                  ''')
-        home(values)
-    elif user == '5':
-        create(values)  
+#           ''')    
+#     user = input('Option > ')
+#     if user == '1':
+#         for num, value in enumerate(values, start=1):
+#             print(f'''
+#                   {num}. {value}
+#                   ''')
+#         home(values)
+#     elif user == '5':
+#         create(values)  
         
-    elif user =='00':
-        exit()
+#     elif user =='00':
+#         exit()
     
-toDo()
+# toDo()
