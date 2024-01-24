00a533528e199ddee848fe506ed1f84d965925fa
Adedokun Solomon <skadedokun16@gmail.com>
2024-01-24 11:29:18 +0100

    seven v5

--- sets.py
+++ sets.py
@@ -34,3 +34,33 @@
 #     # for setItems in no_of_setItems:
 #     #     setItem = int(input('list the numbers in your set: '))
 #     # print(setItem)
+
+# ===============================SET CALCULATOR==========================================
+def home():
+    print('Welcome to set calculator'.title().center(100,'*'))
+    # sets = set(1, 9, 10, 2, 20, )
+    # user = int(input('How many sets are you working with > '))
+    # for sets in range(user):
+    #     set_entry = input(f'Enter name of Set{sets + 1} >: ')
+    #     for set_entries in set_entry:
+    #         name = int(input(f'Enter your Set data >: '))
+    #         print(name)
+    # set().update(set_entries)
+    # print(sets)
+    
+    universalSet = {}
+    inputs = int(input('universal set entries are: '))
+    for user in range(inputs):
+        user = int(input('Enter the items in the Universal set: > '))
+        universalSet.update(user)
+    print(universalSet)
+    data_Items = int(input('How many data Items do you have >: '))
+    while data_Items  > 0:
+        user = int(input('data entry: '))
+        data_Items -= 0
+        print(data_Items)
+        
+        
+    
+    
+home()
